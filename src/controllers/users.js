f1a113ec031e43def391c434eea19bbfbdb98dba
fazt <me@faztweb.com>
2017-10-22 14:14:06 -0500

    async await

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,29 +1,23 @@
 const User = require('../models/user');
 
-// we can interact with mongoose in these ways:
-// 1 - callbacks
-// 2 - promises
-// 3 - async / await (Promises)
-
 module.exports = {
-	index: (req, res, next) => {
-		User.find({})
-			.then(users => {
-				res.status(200).json(users);
-			})
-			.catch(err => {
-				next(err);
-			});
-	},			
+	index: async (req, res, next) => {
+		try {
+			const users = await User.find({});
+			// throw new Error('my dummy error');
+			res.status(200).json(users);
+		} catch (e) {
+			next(e);
+		}
+	},
 
-	newUser: (req, res, next) => {
-		const newUser = new User(req.body);
-		newUser.save()
-			.then(user => {
-				res.status(200).json(user);
-			}) 
-			.catch(err => {
-				next(err);
-			});
+	newUser: async (req, res, next) => {
+		try {
+			const newUser = new User(req.body);
+			const user = await newUser.save();
+			res.status(201).json(user);
+		} catch (e) {
+			next(e);
+		}
 	}
 };
